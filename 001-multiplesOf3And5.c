7510a139a534b930b7f014f8b19523c3befcd028
LeedMx <esparza.mexico@gmail.com>
2018-09-05 17:36:56 -0500

    Solved 001 for HR

--- 001-multiplesOf3And5.c
+++ 001-multiplesOf3And5.c
@@ -1,32 +1,46 @@
-//Primer problema de project Euler
+//Multiples of 3 and 5, Project Euler Problem 1
 /*
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
-#include <stdio.h>
 
-int xEsMultiploDeN(int x,int N);
-int imprimirDosColumnas(int i,int total);
+#include <stdio.h>
+#define printl(ref) printf(#ref" = %lld\n",ref);
+
+#define HACKERRANK 0
+int hackerrank();
+
+long long int triangle(long long int);
 
 int main(){
-	int total=0;
-	printf("iteracion:\t\tTotal:\n");
+
+	if(!HACKERRANK){
+		long long target = 1000, solution = 0;
+		target--;
+		solution = 3*(triangle(target/3)) + 5*(triangle(target/5)) - 15*(triangle(target/15));
+		printl(solution);
+	}
 	
-	for (int i=1; i<1000; i++)
-	if(xEsMultiploDeN(i,3)||xEsMultiploDeN(i,5)){
-		total=total+i;
-		imprimirDosColumnas(i,total);}
+	if(HACKERRANK)hackerrank();
+	
 	return 0;
 }
 
-int xEsMultiploDeN(int x,int N){
-	if (x%N==0)
-		return 1;
-	return 0;
+long long int triangle(long long int n){
+	return n * (n+1)/2;
 }
 
-int imprimirDosColumnas(int a,int b){
-	printf("%d\t\t\t%d\n",a,b);
+int hackerrank(){
+	int testCases=0;
+	long long int target=0,solution=0;
+	scanf("%d",&testCases);
+	
+	for (int test = 1; test <= testCases ; test++){
+		scanf("%lld",&target);
+		target--;
+		solution = 3*(triangle(target/3)) + 5*(triangle(target/5)) - 15*(triangle(target/15));
+		printf("%lld\n",solution);
+	}
 	return 0;
 }
